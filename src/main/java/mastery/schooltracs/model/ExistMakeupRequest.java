7300fefa393a1d5e806a8058d331b2f36ad89014
Travis Li <coolki@gmail.com>
2017-07-28 10:24:16 +0800

    update

--- src/main/java/mastery/schooltracs/model/ExistMakeupRequest.java
+++ src/main/java/mastery/schooltracs/model/ExistMakeupRequest.java
@@ -22,88 +22,12 @@ public class ExistMakeupRequest {
 	
 	private Integer reqSeq;
 	private String task;
-	private Date startTime;
-	private Date endTime;
-	private String staffId;
-	private String facilityId;
-	private String productId;
-	private String id;
-	private String centerId;
-	private String customerId;
+	private String toLsonId;
+	private String fromLsonId;
+	private String unknownNo;
+	private String unknownStr;
 	
-	public ExistMakeupRequest(Integer reqSeq, Lesson l, String custId){
-		this.reqSeq = reqSeq;
-		this.task = SchoolTracsConst.Task.NEW_MAKE_UP.code();
-		this.startTime = l.getStartDateTime();
-		this.endTime = l.getEndDateTime();
-		this.staffId = l.getTeacher().getId();
-		this.facilityId = l.getRoom().getId();
-		this.productId = "";
-		this.id = l.getId();
-		this.centerId = l.getCenterId();
-		this.customerId = custId;
-	}
 	
-	public Integer getReqSeq() {
-		return reqSeq;
-	}
-	public void setReqSeq(Integer reqSeq) {
-		this.reqSeq = reqSeq;
-	}
-	public String getTask() {
-		return task;
-	}
-	public void setTask(String task) {
-		this.task = task;
-	}
-	public Date getStartTime() {
-		return startTime;
-	}
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
-	public Date getEndTime() {
-		return endTime;
-	}
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
-	}
-	public String getStaffId() {
-		return staffId;
-	}
-	public void setStaffId(String staffId) {
-		this.staffId = staffId;
-	}
-	public String getFacilityId() {
-		return facilityId;
-	}
-	public void setFacilityId(String facilityId) {
-		this.facilityId = facilityId;
-	}
-	public String getProductId() {
-		return productId;
-	}
-	public void setProductId(String productId) {
-		this.productId = productId;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getCenterId() {
-		return centerId;
-	}
-	public void setCenterId(String centerId) {
-		this.centerId = centerId;
-	}
-	public String getCustomerId() {
-		return customerId;
-	}
-	public void setCustomerId(String customerId) {
-		this.customerId = customerId;
-	}
 	
 	
 	
