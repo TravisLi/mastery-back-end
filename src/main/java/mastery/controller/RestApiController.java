20f8a5f04d7989fa23309b41e6746227fa97ad2f
Timmy Wong <thcathy@gmail.com>
2017-08-05 10:42:16 +0800

    allow CORS from any domain

--- src/main/java/mastery/controller/RestApiController.java
+++ src/main/java/mastery/controller/RestApiController.java
@@ -1,12 +1,12 @@
 package mastery.controller;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import mastery.model.Auth;
+import mastery.model.Lesson;
+import mastery.model.User;
+import mastery.schooltracs.core.SchoolTracsAgent;
+import mastery.schooltracs.model.Customer;
+import mastery.util.MasteryUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.ClientProtocolException;
 import org.slf4j.Logger;
@@ -14,23 +14,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
-import mastery.model.Auth;
-import mastery.model.Lesson;
-import mastery.model.User;
-import mastery.schooltracs.core.SchoolTracsAgent;
-import mastery.schooltracs.model.Customer;
-import mastery.util.MasteryUtil;
-
-@CrossOrigin(origins = {"http://localhost:4200"}, maxAge = 4800, allowCredentials = "false") 
+@CrossOrigin(maxAge = 4800, allowCredentials = "false")
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
